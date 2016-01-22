1ee3eb5f46bdac0e5611a69c63329d06df1a0065
USAMI Kenta <tadsan@zonu.me>
2016-01-22 21:37:39 +0900

    Init repository

---
+++ psysh.el
@@ -0,0 +1,45 @@
+;;; psysh.el --- PsySH, PHP interactive shell (REPL) -*- lexical-binding: t -*-
+
+;; Copyright (C) 2016 USAMI Kenta
+
+;; Author: USAMI Kenta <tadsan@zonu.me>
+;; Keywords: process, php
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Setup:
+
+;; A: The easy way
+;; $ wget psysh.org/psysh
+;; $ chmod +x psysh
+;; And copy or make symlink to your $PATH dir.
+
+;; B: The other easy way
+;; Get Composer.  See https://getcomposer.org/download/
+;; $ composer g require psy/psysh:@stable
+
+;;; Code:
+
+;;;###autoload
+(defun psysh ()
+  "Run PsySH interactive shell."
+  (interactive)
+  (display-buffer (make-comint "PsySH" "psysh")))
+
+(provide 'psysh)
+;;; psysh.el ends here
