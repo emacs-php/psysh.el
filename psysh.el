6932f03447c3d821e4c03e99f1630928f0979452
USAMI Kenta <tadsan@zonu.me>
2016-07-12 02:47:30 +0900

    Update README

--- psysh.el
+++ psysh.el
@@ -23,16 +23,57 @@
 
 ;;; Commentary:
 
-;; Setup:
-
-;; A: The easy way
-;; $ wget psysh.org/psysh
-;; $ chmod +x psysh
+;; ## Installation
+;;
+;; ### A: The easy way
+;;
+;;     $ wget psysh.org/psysh
+;;     $ chmod +x psysh
+;;
 ;; And copy or make symlink to your $PATH dir.
-
-;; B: The other easy way
+;;
+;;
+;; ### B: The other easy way
+;;
 ;; Get Composer.  See https://getcomposer.org/download/
-;; $ composer g require psy/psysh:@stable
+;;
+;;     $ composer g require psy/psysh:@stable
+;;
+;;
+;; ### C: Project local REPL
+;;
+;; Set `psysh-comint-buffer-process' (buffer local variable).
+;;
+;;     (setq psysh-comint-buffer-process "path/to/shell.php")
+;;
+;; `shell.php' is for example:
+;;
+;;     #!/usr/bin/env php
+;;     <?php
+;;     // ↓Namespace for your project
+;;     namespace Nyaan;
+;;     
+;;     // load Composer autoload file
+;;     require_once __DIR__ . '/vendor/autoload.php';
+;;     // load other initialize PHP files
+;;     // require_once …
+;;     
+;;     echo __NAMESPACE__ . " shell\n";
+;;     
+;;     $sh = new \Psy\Shell();
+;;     
+;;     // Set project namespace in REPL
+;;     if (defined('__NAMESPACE__') && __NAMESPACE__ !== '') {
+;;         $sh->addCode(sprintf("namespace %s;", __NAMESPACE__));
+;;     }
+;;     
+;;     $sh->run();
+;;     
+;;     // Termination message
+;;     echo "Bye.\n";
+;;
+;; See also http://cho-phper.hateblo.jp/entry/2015/11/10/031000 *(Japanese)*
+;;
 
 ;;; Code:
 (require 'comint)
